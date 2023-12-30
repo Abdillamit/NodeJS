ea2f2d07ebda272bb369910ecf52f91b078b1b98
Abdillamit <abdillamit08@gmail.com>
2023-12-30 18:27:08 +0600

    Module Path

--- myApp/modules/path.js
+++ myApp/modules/path.js
@@ -1,12 +1,12 @@
 const path = require('node:path');
 
-// console.log(path.basename(__filename));
-// console.log(path.dirname(__filename));
-// console.log(path.extname(__filename));
-// console.log(path.parse(__filename).dir);
-// console.log(path.dirname(__dirname));
-// console.log(path.extname(__dirname));
+console.log(path.basename(__filename));
+console.log(path.dirname(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename).dir);
+console.log(path.dirname(__dirname));
+console.log(path.extname(__dirname));
 
-// console.log(path.join(__dirname, "templates", 'hi.ts'));
-// console.log(path.resolve(__dirname, "templates", '/index.html'));
+console.log(path.join(__dirname, "templates", 'hi.ts'));
+console.log(path.resolve(__dirname, "templates", '/index.html'));
 console.log(path.resolve(__dirname, "./templates", 'index.html'));
