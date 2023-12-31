0109cbfe7c9a87dd5948fd5f13d19dbbcff4e606
Abdillamit <abdillamit08@gmail.com>
2023-12-31 18:09:04 +0600

    Module OS

--- myApp/modules/fs.js
+++ myApp/modules/fs.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-// fs.mkdir(path.join(__dirname, 'tamplete'), err => {
-//   if (err) throw new Error();
-//   console.log("Done!  - Folder Created :)");
-// })
+fs.mkdir(path.join(__dirname, 'tamplete'), err => {
+  if (err) throw new Error();
+  console.log("Done!  - Folder Created :)");
+})
 
 
-// fs.mkdir(path.join(__dirname, 'notes'), err => {
-//   if (err) throw new Error();
+fs.mkdir(path.join(__dirname, 'notes'), err => {
+  if (err) throw new Error();
 
-//   console.log("Done ;)");
-// })
+  console.log("Done ;)");
+})
 
 fs.writeFile(path.join(__dirname, 'notes', 'note.txt'), 'Lesson NodeJS', err => {
   if (err) throw new Error();
@@ -29,20 +29,20 @@ fs.writeFile(path.join(__dirname, 'notes', 'note.txt'), 'Lesson NodeJS', err => 
   })
 })
 
-// <Buffer 4c 65 73 73 6f 6e 20 4e 6f 64 65 4a 53>
-// fs.readFile(path.join(__dirname, 'notes', 'note.txt'), (err, data) => {
-//   if (err) throw new Error();
+//<Buffer 4c 65 73 73 6f 6e 20 4e 6f 64 65 4a 53>
+fs.readFile(path.join(__dirname, 'notes', 'note.txt'), (err, data) => {
+  if (err) throw new Error();
 
-//   console.log(data);
-// }
-
-
-// Midil
-// fs.readFile(path.join(__dirname, 'notes', 'note.txt'), (err, data) => {
-//   if (err) throw new Error();
-
-//   console.log(Buffer.from(data).toString());
-// })
+  console.log(data);
+},
 
 
+  // Midil
+  fs.readFile(path.join(__dirname, 'notes', 'note.txt'), (err, data) => {
+    if (err) throw new Error();
 
+    console.log(Buffer.from(data).toString());
+  })
+
+
+)
---
+++ myApp/modules/os.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// Platform
+console.log(os.cpus());
+
+// Architecture
+console.log(os.arch());
+
+// Free Memory
+console.log(os.freemem());
+
+// Total Memory
+console.log(os.totalmem());
+
+// Entry Point
+console.log(os.homedir());
